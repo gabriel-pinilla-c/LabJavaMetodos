c42d3828b71cc2fc54d4230ef02c45d3ed547fa4
Arkanhell98 <gabopc98@gmail.com>
2025-05-06 19:43:17 -0500

    Lab Metodos 7

---
+++ LabMetodos/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        //PRIMER EJERCICIO
+
+//
+//        System.out.println("Para convertir Metros a Centímetros digita la cantidad deseada:");
+//        double metros = scanner.nextDouble();
+//
+//        System.out.println("Para convertir de Kilogramos a Libras digita la cantidad deseada:");
+//        double kilogramos = scanner.nextDouble();
+//
+//        System.out.println("Para convertir de Celsius a Fahrenheit digita la cantidad deseada:");
+//        double celsius = scanner.nextDouble();
+//
+//        Ejercicio1.mostrarResultados(metros, kilogramos, celsius);
+
+
+        //SEGUNDO PUNTO
+
+//        System.out.println("Digite la primera nota");
+//        double nota1 = scanner.nextDouble();
+//        System.out.println("Digite la Segunda nota");
+//        double nota2 = scanner.nextDouble();
+//        System.out.println("Digite la Segunda nota");
+//        double nota3 = scanner.nextDouble();
+//        scanner.close();
+//
+//        Ejercicio2.mensaje(nota1,nota2,nota3);
+
+        // TERCER PUNTO
+
+        Ejercicio3.operaciones();
+
+
+    }
+}
